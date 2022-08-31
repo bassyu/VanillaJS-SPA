835f3181b980f6204bbba04932a0957a120bee82
bassyu <yrkdgus@gmail.com>
2022-08-31 20:29:54 +0900

    Modified request

--- src/components/ProductDetailPage.js
+++ src/components/ProductDetailPage.js
@@ -18,7 +18,7 @@ export default function ProductDetailPage({ $target, productId }) {
 
   this.fetchProduct = async () => {
     const { productId } = this.state;
-    const [ product ] = await dummyRequest(`/products/${productId}`);
+    const product = await dummyRequest(`/products/${productId}`);
     this.setState({
       ...this.state,
       product
@@ -33,7 +33,7 @@ export default function ProductDetailPage({ $target, productId }) {
     }
 
     $page.innerHTML = '<h1>상품 정보</h1>';
-    
+
     new ProductDetail({
       $target: $page,
       initialState: {
